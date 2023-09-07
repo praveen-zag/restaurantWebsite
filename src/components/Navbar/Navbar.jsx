192183183e65e5e2a8699466621341992adc8a30
praveen-zag <praveensingh190720@gmail.com>
2023-09-07 14:33:54 +0530

    final2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         <li className="p__opensans"><a href="#about">About</a></li>
         <li className="p__opensans"><a href="#menu">Menu</a></li>
         <li className="p__opensans"><a href="#awards">Awards</a></li>
-        <li className="p__opensans"><a href="#contanct">Contact</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
       </ul>
       <div className="app__navbar-login">
         <a href="#login" className='p__opensans'>Login/Register</a>
